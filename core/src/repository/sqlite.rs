9a686c968ebcfcf1b269b74bbf78e6e968b931de
lilo <linoallg@schafferts.ch>
2026-08-20 20:14:44 +0200

    Added delete and listFromLernset to learitem sqlite

--- core/src/repository/sqlite.rs
+++ core/src/repository/sqlite.rs
@@ -32,15 +32,16 @@ impl LernsetRepository for SqliteLernsetRepository {
     }
     fn delete(&self, id: usize) -> rusqlite::Result<()> {
         let rows_affected = self.conn.execute(
-        "DELETE FROM items WHERE id = ?1",
-        [id as i32],
-    )?;
+        "DELETE FROM lernset WHERE lernset_id = ?1",
+        [id as i64],
+        )?;
 
-    if rows_affected == 0 {
-        return Err(rusqlite::Error::QueryReturnedNoRows);
+        if rows_affected == 0 {
+            return Err(rusqlite::Error::QueryReturnedNoRows);
+        }
+
+        Ok(())
     }
-
-    Ok(())}
     fn list(&self) -> rusqlite::Result<Vec<Lernset>> {
         let mut stmt = self.conn.prepare("SELECT id, name FROM lernset")?;
         let lernset_iter = stmt.query_map([], |row| {
@@ -86,21 +87,61 @@ impl LearnitemRepository for SqlLIteLearnitemRepository {
        self.conn.query_row("SELECT learnitem_id, lernset_id, origin_meaning, trans_meaning, learnstate, remaining FROM learnitem WHERE learnitem_id = (?1)", 
             params![learnitem_id as i64],
             |row| {
-                let learnstate = match row.get(4)? as &str {
-                    "Finished" => Learnstate::Finished,
-                    "NotStarted" => Learnstate::NotStarted,
-                    "Learning" => {
-                        let rem = row.get(5)? as usize;
-                        Learnstate::Learning(rem)
-                    }
-                };
+                let state: String = row.get(4)?;
+                let rem: Option<i64> = row.get(5)?;
+                let learnstate = SqlLIteLearnitemRepository::conv_state(state.as_str(), rem)?;
+
+                let lern_id: i64 = row.get(1)?;
                 Ok(Learnitem {
                     learnitem_id,
-                    lernset_id: row.get(1)? as usize,
+                    lernset_id: lern_id as usize,
                     origin_meaning: row.get(2)?,
                     trans_meaning: row.get(3)?,
                     learnstate,
                 })
         })
+    }
+    fn delete(&self, id: usize) -> rusqlite::Result<()> {
+        let rows_affected = self.conn.execute(
+            "DELETE FROM learnitem WHERE learnitem_id",
+            [id as i64]
+        )?;
+        if rows_affected == 0 {
+            return Err(rusqlite::Error::QueryReturnedNoRows);
+        }
+        Ok(())
+    }
+    fn list_from_lernset(&self, lernset_id: usize) -> rusqlite::Result<Vec<Learnitem>> {
+        let mut stmt = self.conn.prepare("SELECT learnitem_id, lernset_id, origin_meaning, trans_meaning, learnstate, remaining FROM learnitem WHERE lernset_id = (?1)")?;
+        let rows = stmt.query_map(params![lernset_id as i64],
+            |row| {
+                let state: String = row.get(4)?;
+                let rem: Option<i64> = row.get(5)?;
+                let learnstate = SqlLIteLearnitemRepository::conv_state(state.as_str(), rem)?;
+
+                let row_learnitem_id: i64 = row.get(0)?;
+                let row_lernset_id: i64 = row.get(1)?;
+
+                Ok(Learnitem {
+                    learnitem_id: row_learnitem_id as usize,
+                    lernset_id: row_lernset_id as usize,
+                    origin_meaning: row.get(2)?,
+                    trans_meaning: row.get(3)?,
+                    learnstate
+                })
+            })?;
+        rows.collect::<rusqlite::Result<Vec<_>>>()
+    }
+}
+impl SqlLIteLearnitemRepository {
+    fn conv_state(state: &str, rem: Option<i64>) -> rusqlite::Result<Learnstate> {
+        match state {
+            "Finished" => return Ok(Learnstate::Finished),
+            "NotStarted" => return Ok(Learnstate::NotStarted),
+            "Learning" => {
+                return Ok(Learnstate::Learning(rem.unwrap_or(5) as usize))
+            },
+            _ => return Err(rusqlite::Error::IntegralValueOutOfRange(5, rem.unwrap_or(5)))
+        };
     }
 }
