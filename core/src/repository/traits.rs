d03ea5733f1d6fd68a09f94bc947c98b7ed27b96
lilo <linoallg@schafferts.ch>
2026-08-21 16:40:10 +0200

    Added update to traits.

--- core/src/repository/traits.rs
+++ core/src/repository/traits.rs
@@ -6,6 +6,7 @@ pub trait LernsetRepository {
     fn get(&self, id: usize) -> Result<Lernset>;
     fn list(&self) -> Result<Vec<Lernset>>;
     fn delete(&self, id: usize) -> Result<()>;
+    fn update(&self, lernset: &Lernset) -> Result<()>;
 }
 
 pub trait LearnitemRepository {
@@ -13,4 +14,5 @@ pub trait LearnitemRepository {
     fn list_from_lernset(&self, lernset_id: usize) -> Result<Vec<Learnitem>>;
     fn get(&self, learnitem_id: usize) -> Result<Learnitem>;
     fn delete(&self, id: usize) -> Result<()>;
+    fn update(&self, learnitem: &Learnitem) -> Result<()>;
 }
